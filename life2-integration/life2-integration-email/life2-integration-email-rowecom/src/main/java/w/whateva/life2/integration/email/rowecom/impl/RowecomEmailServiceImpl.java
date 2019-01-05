e741bf0291b565ab1ff2528ba2528e299ea9ea8c
billshwah47 <bill_shwah@yahoo.com>
2019-01-05 13:25:09 -0500

    dynamically build email ArtifactProviders looping through EmailOperation clients

--- life2-integration/life2-integration-email/life2-integration-email-rowecom/src/main/java/w/whateva/life2/integration/email/rowecom/impl/RowecomEmailServiceImpl.java
+++ life2-integration/life2-integration-email/life2-integration-email-rowecom/src/main/java/w/whateva/life2/integration/email/rowecom/impl/RowecomEmailServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Service
+@Deprecated
 @EnableFeignClients(basePackageClasses = RowecomEmailClient.class)
 public class RowecomEmailServiceImpl implements ArtifactProvider {
 
--- life2-service/life2-service-artifact/src/main/java/w/whateva/life2/service/artifact/impl/ArtifactServiceImpl.java
+++ life2-service/life2-service-artifact/src/main/java/w/whateva/life2/service/artifact/impl/ArtifactServiceImpl.java
@@ -2,17 +2,22 @@ package w.whateva.life2.service.artifact.impl;
 
 import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.support.GenericWebApplicationContext;
 import w.whateva.life2.api.common.ArtifactOperations;
 import w.whateva.life2.api.common.dto.ApiArtifact;
-import w.whateva.life2.service.artifact.util.ArtifactUtility;
+import w.whateva.life2.api.email.EmailOperations;
 import w.whateva.life2.integration.api.ArtifactProvider;
+import w.whateva.life2.integration.email.impl.EmailProviderImpl;
+import w.whateva.life2.service.artifact.util.ArtifactUtility;
 import w.whateva.life2.service.artifact.util.bucket.AbstractLocalDateTimeOperator;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,15 +27,25 @@ import java.util.stream.Collectors;
  */
 @Primary
 @Service
+@EnableFeignClients(basePackages = "w.whateva.life2.integration")
 public class ArtifactServiceImpl implements ArtifactOperations {
 
-    private final List<ArtifactProvider> providers;
     private final ArtifactUtility artifactUtility;
 
+    private final Collection<ArtifactProvider> providers = Lists.newArrayList();
+
     @Autowired
-    public ArtifactServiceImpl(List<ArtifactProvider> providers, ArtifactUtility artifactUtility) {
-        this.providers = providers;
+    public ArtifactServiceImpl(GenericWebApplicationContext context, ArtifactUtility artifactUtility) {
+
         this.artifactUtility = artifactUtility;
+
+        // dynamically register an artifact provider for each email client (EmailOperations extensions are FeignClient)
+        int i = 0;
+        for (EmailOperations client : context.getBeansOfType(EmailOperations.class).values()) {
+            context.registerBean("emailProvider" + i++, ArtifactProvider.class, () -> new EmailProviderImpl(client));
+        }
+
+        providers.addAll(context.getBeansOfType(ArtifactProvider.class).values());
     }
 
     @Override
