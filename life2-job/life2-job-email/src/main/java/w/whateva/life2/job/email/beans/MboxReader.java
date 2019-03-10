b8bae03cd2af035faeb47244a26969d13e9b820b
billshwah47 <bill_shwah@yahoo.com>
2019-03-10 16:11:42 -0400

    some alterations to the reader to attempt to parse messages from forward trails

--- life2-job/life2-job-email/src/main/java/w/whateva/life2/job/email/beans/MboxReader.java
+++ life2-job/life2-job-email/src/main/java/w/whateva/life2/job/email/beans/MboxReader.java
@@ -1,13 +1,8 @@
 package w.whateva.life2.job.email.beans;
 
-import org.apache.tika.extractor.EmbeddedDocumentExtractor;
-import org.apache.tika.extractor.EmbeddedDocumentUtil;
+import org.apache.commons.lang.StringUtils;
 import org.apache.tika.io.IOUtils;
-import org.apache.tika.metadata.Metadata;
-import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.mbox.MboxParser;
-import org.apache.tika.sax.BodyContentHandler;
-import org.apache.tika.sax.XHTMLContentHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemReader;
@@ -24,19 +19,20 @@ public class MboxReader extends MboxParser implements ItemReader<MimeMessage> {
 
     private transient Logger log = LoggerFactory.getLogger(MboxReader.class);
 
+    private static final int MAIL_INIT_SIZE = 100000;
     private static final int MAIL_MAX_SIZE = 100000000;
 
     private static final String charsetName = "utf-8";
 
-    private final BodyContentHandler handler;
-    private final EmbeddedDocumentExtractor extractor;
     private final BufferedReader reader;
     private String curLine;
+    private ByteArrayOutputStream message = new ByteArrayOutputStream(MAIL_INIT_SIZE);
+    private Stack<EmbeddedMessage> currentEmbeddedMessages = new Stack<>();
+    private Stack<EmbeddedMessage> embeddedMessages = new Stack<>();
+    private List<String> mimeMessages = new ArrayList<>();
 
     public MboxReader(InputStream inputStream) throws IOException {
 
-        this.handler = new BodyContentHandler(MAIL_MAX_SIZE);
-        this.extractor = EmbeddedDocumentUtil.getEmbeddedDocumentExtractor(new ParseContext());
         try {
             Reader streamReader = new InputStreamReader(inputStream, charsetName);
             this.reader = new BufferedReader(streamReader);
@@ -49,39 +45,65 @@ public class MboxReader extends MboxParser implements ItemReader<MimeMessage> {
     @Override
     public MimeMessage read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
 
+        /*
+        if (currentEmbeddedMessages.size() != 0) {
+            log.warn(String.format("%d dangling embedded messages...", currentEmbeddedMessages.size()));
+            return buildMimeMessage(currentEmbeddedMessages.pop().outputStream);
+        }
+
+        if (embeddedMessages.size() != 0) {
+            return buildMimeMessage(embeddedMessages.pop().outputStream);
+        }
+        */
+
         if (curLine == null || Thread.currentThread().isInterrupted()) {
 
             return close();
 
         } else if (curLine.startsWith(MBOX_RECORD_DIVIDER)) {
 
-            Metadata mailMetadata = new Metadata();
-
-            XHTMLContentHandler xhtml = new XHTMLContentHandler(handler, mailMetadata);
-            xhtml.startDocument();
-
             if (curLine == null) {
                 return close();
             }
 
-            ByteArrayOutputStream message = new ByteArrayOutputStream(100000);
             do {
                 message.write(curLine.getBytes(charsetName));
                 message.write('\n');
+
+                /*
+                if (null != embeddedMessageStart(curLine)) {
+                    currentEmbeddedMessages.add(new EmbeddedMessage(embeddedMessageStart(curLine)));
+                } else if (EmbeddedMessageFrame.FORWARDED.equals(embeddedMessageEnd(curLine))) {
+
+                    List<EmbeddedMessage> children = new ArrayList<>();
+
+                    // close off all embedded messages until you get back to the forwarded one...
+                    while (!currentEmbeddedMessages.isEmpty()) {
+                        EmbeddedMessage currentEmbeddedMessage = currentEmbeddedMessages.pop();
+                        embeddedMessages.add(currentEmbeddedMessage);
+                        if (EmbeddedMessageFrame.FORWARDED.equals(currentEmbeddedMessage.frame)) {
+                            while (!currentEmbeddedMessages.isEmpty() && EmbeddedMessageFrame.FORWARDED.equals(currentEmbeddedMessages.peek().frame)) {
+                                children.add(currentEmbeddedMessages.pop()); // get layered forwards
+                            }
+                            currentEmbeddedMessage.setChildren(children);
+                            break;
+                        } else {
+                            children.add(currentEmbeddedMessage);
+                        }
+                    }
+                }
+
+                for (EmbeddedMessage currentEmbeddedMessage : currentEmbeddedMessages) {
+                    currentEmbeddedMessage.write(curLine);
+                }
+                */
+
                 curLine = reader.readLine();
             }
             while (curLine != null && !curLine.startsWith(MBOX_RECORD_DIVIDER) && message.size() < MAIL_MAX_SIZE);
 
-            ByteArrayInputStream messageStream = new ByteArrayInputStream(message.toByteArray());
-
-            if (extractor.shouldParseEmbedded(mailMetadata)) {
-                extractor.parseEmbedded(messageStream, xhtml, mailMetadata, true);
-            }
-
-            xhtml.endDocument();
-
             try {
-                return buildMimeMessage(messageStream);
+                return buildMimeMessage(message);
             } catch (Exception e) {
                 log.error("Could not build mime message from message stream");
             }
@@ -95,8 +117,13 @@ public class MboxReader extends MboxParser implements ItemReader<MimeMessage> {
         return close();
     }
 
+    private MimeMessage buildMimeMessage(ByteArrayOutputStream s) throws Exception {
+        return buildMimeMessage(new ByteArrayInputStream(s.toByteArray()));
+    }
+
     private MimeMessage buildMimeMessage(ByteArrayInputStream s) throws Exception {
         String content = IOUtils.toString(s);
+        mimeMessages.add(content);
         Session session = Session.getInstance(new Properties());
         InputStream is = new ByteArrayInputStream(content.getBytes());
         return new MimeMessage(session, is);
@@ -105,5 +132,73 @@ public class MboxReader extends MboxParser implements ItemReader<MimeMessage> {
     private MimeMessage close() {
         IOUtils.closeQuietly(reader);
         return null;
+    }
+
+    private static EmbeddedMessageFrame embeddedMessageStart(String line) {
+        if (line.startsWith("--- Forwarded mail")) return EmbeddedMessageFrame.FORWARDED;
+        if (line.startsWith("-----Original Message")) return EmbeddedMessageFrame.ORIGINAL;
+        if (line.startsWith("------------------------[ Original Message ]")) return EmbeddedMessageFrame.ORIGINAL;
+        return null;
+    }
+
+    private static EmbeddedMessageFrame embeddedMessageEnd(String line) {
+        if (line.startsWith("---End of forwarded mail from")) return EmbeddedMessageFrame.FORWARDED;
+        return null;
+    }
+
+    static enum EmbeddedMessageFrame {
+        FORWARDED,
+        ORIGINAL,
+        WHITESPACE
+    }
+
+    static class EmbeddedMessage {
+
+        private final EmbeddedMessageFrame frame;
+        private final ByteArrayOutputStream outputStream;
+        private boolean initialized = false;
+        private boolean headers = true;
+        private List<EmbeddedMessage> children;
+
+        EmbeddedMessage(EmbeddedMessageFrame frame) {
+            this.frame = frame;
+            this.outputStream = new ByteArrayOutputStream();
+        }
+
+        void write(String line) {
+
+            if (!initialized && StringUtils.isEmpty(line.trim())) return;
+
+            try {
+                initialized = true;
+                if (headers && null != embeddedMessageStart(line)) {
+                    return;
+                } else if (headers && line.startsWith("From:")) {
+                    line = line.replace("From:", "From\t:");
+                } else if (headers && line.startsWith("To:")) {
+                    line = line.replace("To:", "To\t:");
+                } else if (headers && line.startsWith("Date:")) {
+                    line = line.replace("Date:", "Date\t:");
+                } else if (headers && line.startsWith("Cc:")) {
+                    line = line.replace("Cc:", "Cc\t:");
+                } else if (headers && line.startsWith("Subject:")) {
+                    line = line.replace("Subject:", "Subject\t:");
+                } else {
+                    headers = false;
+                }
+                outputStream.write(line.getBytes(charsetName));
+                outputStream.write('\n');
+            } catch (IOException e) {
+
+            }
+        }
+
+        public List<EmbeddedMessage> getChildren() {
+            return children;
+        }
+
+        public void setChildren(List<EmbeddedMessage> children) {
+            this.children = children;
+        }
     }
 }
